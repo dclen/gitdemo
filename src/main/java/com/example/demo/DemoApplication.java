b316e87f054b97577c7a913fa705d41477cc90bf
Dermot Clenaghan <dclenaghan@hotmail.com>
2021-10-25 11:27:28 +0000

    remove spring

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,7 +14,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @GetMapping("/hello-spring")
+    @GetMapping("/hello")
     String greet(){
         return "Hello, Spring!";
     }
